46bb84d72cd69c2be628c3d400bceea9460bb9c3
DESKTOP-M98ST0G\david <davidtheo@gmail.com>
2023-11-07 13:36:52 +0000

    Add try, except block

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 table = pandas.DataFrame(alphabet)
 nato_alphabet = {row.letter:row.code for (index, row) in table.iterrows()}
 
-# Ask the user for their name
-name = input("Enter your Name ").upper()
+try_input = True
+while try_input:
+    try:
+        # Ask the user for their name
+        name = input("Enter your Name ").upper()
+        if len(name) == 0 or not name.isalpha():
+            raise ValueError("Please input a string")
 
-#search list for letters in the name
-word = [nato_alphabet[n] for n in name if n in nato_alphabet]
-
-#print the Nato words
-print(word)
+    except ValueError as massage:
+        print(massage)
+    else:
+        #search list for letters in the name
+        word = [nato_alphabet[n] for n in name if n in nato_alphabet]
+        try_input = False
+        #print the Nato words
+        print(word)
